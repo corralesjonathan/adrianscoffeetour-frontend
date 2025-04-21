1f15971edbbb03afa4665371555e58a158745445
Jonathan <143845545+corralesjonathan@users.noreply.github.com>
2025-04-21 00:45:30 -0600

    features photos and icons added

--- src/data/features.js
+++ src/data/features.js
@@ -7,6 +7,51 @@ const features = [
         image: "/imgs/feature_1.webp",
         color: "#C92E17"
     },
+
+    {
+        id: 2,
+        title: "The Coffee Transformation",
+        description: "Experience the coffee journey from pulping to roasting.",
+        icon: "/icons/feature_2.svg",
+        image: "/imgs/feature_2.webp",
+        color: "#276E2F"
+    },
+    
+    {
+        id: 3,
+        title: "The Art of Coffee Drying",
+        description: "Learn about traditional drying techniques and artisanal shelling.",
+        icon: "/icons/feature_3.svg",
+        image: "/imgs/feature_3.webp",
+        color: "#214594"
+    },
+
+    {
+        id: 4,
+        title: "Behind the Scenes",
+        description: "Explore the roasting and packaging room, where coffee is prepared for the world.",
+        icon: "/icons/feature_4.svg",
+        image: "/imgs/feature_4.webp",
+        color: "#F5B71F"
+    },
+
+    {
+        id: 5,
+        title: "Breathtaking Landscapes",
+        description: "Admire the stunning mountains and lush gardens surrounding the coffee farm.",
+        icon: "/icons/feature_5.svg",
+        image: "/imgs/feature_5.webp",        
+        color: "#F07E2B"
+    },
+
+    {
+        id: 6,
+        title: "Taste & Enjoy",
+        description: "Savor a perfectly brewed cup of coffee with a traditional snack.",        
+        icon: "/icons/feature_6.svg",
+        image: "/imgs/feature_6.webp",
+        color: "#37251C"
+    },
 ];
 
 export default features
