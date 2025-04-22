cd35d2b62938fafa753a976fb4f57c31679aa81a
Jonathan <143845545+corralesjonathan@users.noreply.github.com>
2025-04-22 14:45:04 -0600

    update btn

--- src/components/Secondary_btn.jsx
+++ src/components/Secondary_btn.jsx
@@ -2,12 +2,19 @@ import { Link } from "react-router-dom";
 export function Secondary_btn( {link, text} ) {
     return (
         <>
-            <Link
-                to={link}
-                className="cursor-pointer flex justify-center items-center px-[20px] py-[10px] font-secondary text-[18px] font-regular rounded-full border-adrians-red border-[1px] text-adrians-red
-                hover:bg-adrians-red hover:text-white
-                transition-all duration-300 ease-in-out">{text}
-            </Link>
+            <div
+                data-aos="fade-up"
+                data-aos-once="true"
+                data-aos-duration="1000"
+                data-aos-delay="400"
+            >
+                <Link
+                    to={link}
+                    className="cursor-pointer flex justify-center items-center px-[20px] py-[10px] font-secondary text-[18px] font-regular rounded-full border-adrians-red border-[1px] text-adrians-red
+                    hover:bg-adrians-red hover:text-white
+                    transition-all duration-300 ease-in-out">{text}
+                </Link>
+            </div>
         </>
     );
 }
