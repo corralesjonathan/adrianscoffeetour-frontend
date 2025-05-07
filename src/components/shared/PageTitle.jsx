9d12905a3a954f2021261c4ae33e1c40e0def4d4
corralesjonathan <corralesrjonathan@gmail.com>
2025-05-06 19:27:25 -0600

    adjust page title for mobile

--- src/components/shared/PageTitle.jsx
+++ src/components/shared/PageTitle.jsx
@@ -1,6 +1,9 @@
 export function PageTitle( {page, title, text} ) {
     return (
-        <div className="flex flex-col items-center justify-center w-[80vw] m-auto py-[40px] mt-[120px]">
+        <div 
+            className="flex flex-col items-center justify-center text-center w-[80vw] m-auto py-[40px] mt-[120px]
+            max-xl:w-[90vw]
+            ">
             <h3 
                 data-aos="fade-up"
                 data-aos-duration="1000"
@@ -13,7 +16,7 @@ export function PageTitle( {page, title, text} ) {
                 data-aos-duration="1000"
                 data-aos-once="true"
                 data-aos-delay="200"
-                className="text-[84px] font-bold font-primary text-adrians-brown">
+                className="text-[84px] font-bold font-primary text-adrians-brown max-xl:text-[64px]">
                 {title}
             </h2>
             <p
