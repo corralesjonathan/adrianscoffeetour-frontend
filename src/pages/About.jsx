45d0b8e1abc6f1f97950a21858e720afeae03e88
corralesjonathan <corralesrjonathan@gmail.com>
2025-05-06 23:50:23 -0600

    about page finished

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ import { PageTitle } from "../components/shared/PageTitle.jsx";
 import { Origins } from "../components/about/Origins.jsx";
 import { Family } from "../components/about/Family.jsx";
 import { Company } from "../components/about/Company.jsx";
+import { Newsletter } from "../components/about/Newsletter.jsx";
+import { Footer } from "../components/shared/Footer.jsx";
 export function About() {
     useEffect(() => {
         document.title = "About Us - Adrian's Coffee Tour";
@@ -16,6 +18,8 @@ export function About() {
             <Origins />
             <Family />
             <Company />
+            <Newsletter />
+            <Footer />
         </>
     )
 }
